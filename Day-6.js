3c281d338f7ed7bded26afbd324a82037c25f538
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-01-04 23:25:56 +0200

    Complete day 6

--- Day-6.js
+++ Day-6.js
@@ -2210,16 +2210,52 @@ kn
 nk
 nuk`;
 
+//part 1 solution
 let groups = input.split(/\n\n/);
 
+// let counter = 0;
+
+// groups.forEach((group) => {
+//   let set = new Set();
+//   for (let i = 0; i < group.length; i++) {
+//     if (group[i] !== "\n") set.add(group[i]);
+//   }
+//   counter += set.size;
+// });
+
+// console.log(counter);
+
+//part 2 solution
+
 let counter = 0;
+groups = groups.map((group) => group.split("\n"));
 
-groups.forEach((group) => {
-  let set = new Set();
-  for (let i = 0; i < group.length; i++) {
-    if (group[i] !== "\n") set.add(group[i]);
+let persons = [];
+let dicts = [];
+for (let i = 0; i < groups.length; i++) {
+  let currentGroup = groups[i];
+  let dict = {};
+  let count = 0;
+  for (let j = 0; j < currentGroup.length; j++) {
+    count++;
+    let currentPerson = currentGroup[j];
+    for (let x = 0; x < currentPerson.length; x++) {
+      let currentLetter = currentPerson[x];
+      if (dict[currentLetter] === undefined) {
+        dict[currentLetter] = 1;
+      } else {
+        dict[currentLetter]++;
+      }
+    }
+    persons[i] = count;
   }
-  counter += set.size;
-});
-
+  dicts.push(dict);
+}
+for (let i = 0; i < dicts.length; i++) {
+  for (let j = 0; j < Object.entries(dicts[i]).length; j++) {
+    if (Object.entries(dicts[i])[j][1] === persons[i]) {
+      counter++;
+    }
+  }
+}
 console.log(counter);
