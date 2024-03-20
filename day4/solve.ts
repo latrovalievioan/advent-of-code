159dfb15bd7f2d619ebbb0f99e4ba05706a3d4c9
Ioan Latrovaliev <latrovalievioan@gmail.com>
2024-03-20 10:45:48 +0200

    solve day4 part2

--- day4/solve.ts
+++ day4/solve.ts
@@ -23,4 +23,36 @@ const part1 = fs
         0,
     );
 
-console.log(part1);
+const part2 = () => {
+    const matching = fs
+        .readFileSync("./input", "utf-8")
+        .split("\n")
+        .filter(Boolean)
+        .map((x) =>
+            x
+                .replace(/Card [0-9]: /, "")
+                .split("|")
+                .map((s) =>
+                    s
+                        .trim()
+                        .split(" ")
+                        .filter(Boolean)
+                        .map((c) => Number(c)),
+                ),
+        )
+        .map((xs) => xs[1].filter((x) => xs[0].includes(x)).length);
+
+    const arr = new Array(matching.length).fill(1);
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = arr[i]; j > 0; j--) {
+            for (let z = i + 1; z <= i + matching[i]; z++) {
+                arr[z]++;
+            }
+        }
+    }
+
+    return arr.reduce((a, b) => a + b, 0);
+};
+
+console.log(part2());
