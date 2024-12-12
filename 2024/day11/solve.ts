aba67fc061168d47997452918193ea84b17538ce
Ioan Latrovaliev <latrovalievioan@gmail.com>
2024-12-12 16:48:35 +0200

    solve part 2

--- 2024/day11/solve.ts
+++ 2024/day11/solve.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs'
 const test_input = fs.readFileSync('./test_input', 'utf-8').trim().split(' ').map(Number)
 const input = fs.readFileSync('./input', 'utf-8').trim().split(' ').map(Number)
 
-const step = (xs: number[]) => xs.flatMap(x => {
-    if(x === 0) return 1
+const blink = (x: number) => {
+    if(x === 0) return [1]
 
     const stringX = x.toString(10)
     if(stringX.length % 2 === 0) {
@@ -13,16 +13,38 @@ const step = (xs: number[]) => xs.flatMap(x => {
         return [Number(stringX.slice(0, mid)), Number(stringX.slice(mid, stringX.length))]
     }
 
-    return x * 2024
-})
-
-const p1 = (xs: number[]) => {
-    for(let i = 0; i < 25; i++) {
-        xs = step(xs)   
-    }
-
-    return xs.length
+    return [x * 2024]
 }
 
 
-console.log(p1([0]))
+type Depth = number;
+type Val = number;
+type Res = number;
+
+const m = new Map<Depth, Map<Val, Res>>()
+
+const solve = (xs: number[], depth: number) => {
+    if(depth === 0) return xs.length
+
+    if(!m.get(depth)) {
+        m.set(depth, new Map())
+    }
+
+    let acc = 0
+    for(let i = 0; i < xs.length; i++){
+        const currDepthMap = m.get(depth)
+
+        if(currDepthMap.has(xs[i])) {
+            acc += currDepthMap.get(xs[i])
+        } else {
+            currDepthMap.set(xs[i], solve(blink(xs[i]), depth - 1))
+
+            acc += currDepthMap.get(xs[i])
+        }
+    }
+
+    return acc
+}
+
+console.log(solve(input, 25))
+console.log(solve(input, 75))
