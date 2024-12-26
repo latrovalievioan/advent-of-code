0d95f737cdf6496abc47f3d95318becc20d15034
Ioan Latrovaliev <latrovalievioan@gmail.com>
2024-12-26 11:48:26 +0200

    day24 p2 try

--- 2024/day24/solve.ts
+++ 2024/day24/solve.ts
@@ -49,8 +49,6 @@ while (operations.length) {
     }
 }
 
-console.log(valuesMap);
-
 const p1 = parseInt(Array.from(valuesMap.entries())
     .filter((kv) => kv[0].startsWith("z"))
     .sort((kvA, kvB) => kvA[0].localeCompare(kvB[0]))
@@ -58,3 +56,46 @@ const p1 = parseInt(Array.from(valuesMap.entries())
     .reduce((acc, curr) => acc + curr[1],''), 2)
 
 console.log(p1);
+
+const xs = Array.from(valuesMap.entries())
+    .filter((kv) => kv[0].startsWith("x"))
+    .sort((kvA, kvB) => kvA[0].localeCompare(kvB[0]))
+
+const ys = Array.from(valuesMap.entries())
+    .filter((kv) => kv[0].startsWith("y"))
+    .sort((kvA, kvB) => kvA[0].localeCompare(kvB[0]))
+
+const zs = Array.from(valuesMap.entries())
+    .filter((kv) => kv[0].startsWith("z"))
+    .sort((kvA, kvB) => kvA[0].localeCompare(kvB[0]))
+
+const xsYsSym = (parseInt(xs.map(x => x[1]).join(''), 2) + parseInt(ys.map(y => y[1]).join(''), 2)).toString(2)
+console.log(xsYsSym)
+console.log(zs.map(z => z[1]).join(''))
+
+const operations2 = input[1]
+    .split("\n")
+    .filter(Boolean)
+    .map((op) => op.replace("-> ", "").split(" "))
+    .map((op) => ({
+        firstOperand: op[0],
+        operation: op[1],
+        secondOperand: op[2],
+        resultKey: op[3],
+    }));
+
+const xyAndOperations = Object.values(operations2)
+    .filter((v) => v.operation === "AND")
+    .filter(
+        (v) =>
+            v.firstOperand.startsWith("x") || v.secondOperand.startsWith("y"),
+    ).map(v => {
+        const operationResult = valuesMap.get(v.firstOperand) & valuesMap.get(v.secondOperand)
+
+        return {...v, operationResult, resultKeyValue: valuesMap.get(v.resultKey)}
+    })
+
+//
+// // console.log(zipped)
+// console.log(valuesMap)
+
