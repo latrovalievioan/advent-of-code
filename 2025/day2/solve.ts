9371a0e6a597ceb4870167b1fa805360ffc36f46
Ioan Latrovaliev <latrovalievioan@gmail.com>
2025-12-02 10:39:54 +0200

    solve 2025 day 2 p 2

--- 2025/day2/solve.ts
+++ 2025/day2/solve.ts
@@ -1,6 +1,30 @@
 import * as fs from 'fs'
 
-const p1 = fs.readFileSync("./01", "utf8")
+// const p1 = fs.readFileSync("./01", "utf8")
+//     .replace(/\n/g, '')
+//     .split(",")
+//     .map(s => s.split('-').map(x => Number(x)))
+//     .flatMap(range => {
+//         const [l, r] = range
+//
+//         const invalidIds: number[] = []
+//
+//         for (let i = l; i <= r; i++) {
+//             if (i.toString(10).length % 2 !== 0) continue
+//
+//             const toStr = i.toString(10)
+//
+//             if (toStr.substring(0, toStr.length / 2) === toStr.substring(toStr.length / 2, toStr.length)) {
+//                 invalidIds.push(i)
+//             }
+//         }
+//
+//         return invalidIds
+//     }).reduce((a, c) => a + c)
+//
+// console.log(p1)
+
+const p2 = fs.readFileSync("./01", "utf8")
     .replace(/\n/g, '')
     .split(",")
     .map(s => s.split('-').map(x => Number(x)))
@@ -10,16 +34,34 @@ const p1 = fs.readFileSync("./01", "utf8")
         const invalidIds: number[] = []
 
         for (let i = l; i <= r; i++) {
-            if (i.toString(10).length % 2 !== 0) continue
-
             const toStr = i.toString(10)
 
-            if (toStr.substring(0, toStr.length / 2) === toStr.substring(toStr.length / 2, toStr.length)) {
-                invalidIds.push(i)
+            const mid = toStr.length / 2
+
+            for(let j = 1; j <= mid; j++) {
+                const pattern = toStr.substring(0, j)
+
+                if(toStr.length % pattern.length !== 0) continue
+
+                let isMatching = true
+
+                for(let k = 0; k <= toStr.length - j; k += j) {
+                    if(toStr.substring(k, k + j) !== pattern) {
+                        isMatching = false
+                    }      
+                }
+
+                if(isMatching) {
+                    invalidIds.push(i)
+
+                    break
+                }
             }
         }
 
-        return invalidIds
-    }).reduce((a, c) => a + c)
+        console.log(invalidIds)
 
-console.log(p1)
+        return invalidIds
+    }).reduce((a, c) => a + c, 0)
+
+console.log(p2)
